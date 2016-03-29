774c7a5ff36e0ca09fd17db1e94ecd11dc9b17d0
Chau Tsun Man <tsunman.chau96@gmail.com>
2016-03-29 15:16:21 +0800

    Udacity: JavaScript Basics: Data Types Format Data, Build a Resume Header modified/solution
    re-organized js/resumeBuilder.js for long-term development

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,17 +2,19 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 // Udacity: JavaScript Basics: Data Types: Format Data, Build a Resume Header
-var formattedName = HTMLheaderName.replace("%data%", "Man");
-var formattedRole = HTMLheaderRole.replace("%data%", "Software Engineer");
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
+var name = "Man";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var role = "Software Engineer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+/* Udacity: JavaScript Basics: Getting Up and Running: Try .append() for Yourself
+$("#main").append("Man");
+*/
 
 /* Udacity: JavaScript Basics: Data Types: string.replace()
 var awesomeThoughts = "I am Man and I am AWESOME!";
 var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 $("#main").append(funThoughts);
-*/
-
-/* Udacity: JavaScript Basics: Getting Up and Running: Try .append() for Yourself
-$("#main").append("Man");
 */
