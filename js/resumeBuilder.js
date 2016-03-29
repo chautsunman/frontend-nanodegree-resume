064af5a5809c889373a1922d862463a54cee7bed
Chau Tsun Man <tsunman.chau96@gmail.com>
2016-03-29 14:56:51 +0800

    Udacity: JavaScript Basics: Data Types string.replace()

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append("Man");
+// $("#main").append("Man");
+
+var awesomeThoughts = "I am Man and I am AWESOME!";
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+$("#main").append(funThoughts);
